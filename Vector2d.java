8bdfe3b6670d5b11ecf1a827ca4b281bf93835ce
CodingLegend0100 <117676748+HagenSV@users.noreply.github.com>
2024-10-01 19:20:36 -0400

    Vector class with basic vector functions

---
+++ Vector2d.java
@@ -0,0 +1,26 @@
+public class Vector2d {
+    float x;
+    float y;
+
+    public Vector2d(){
+        x = 0;
+        y = 0;
+    }
+
+    public Vector2d(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public float getMagnitude(){
+        return (float)Math.sqrt((x*x)+(y*y));
+    }
+
+    public Vector2d add(Vector2d other){
+        return new Vector2d(this.x+other.x,this.y+other.y);
+    }
+
+    public Vector2d multiply(float scalar){
+        return new Vector2d(this.x*scalar,this.y*scalar);
+    }
+}
