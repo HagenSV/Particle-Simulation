cbe3b8595e9481be7280ec95f566d6a73134c354
CodingLegend0100 <117676748+HagenSV@users.noreply.github.com>
2024-10-01 19:17:31 -0400

    Draw particles to screen

--- GraphicsPanel.java
+++ GraphicsPanel.java
@@ -1,4 +1,7 @@
 import java.awt.BorderLayout;
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -8,11 +11,26 @@ import javax.swing.JPanel;
  */
 public class GraphicsPanel extends JPanel {
 
+    private List<Particle> particles;
+
+    public GraphicsPanel(){
+        particles = new ArrayList<>();
+        particles.add(new Particle(50, 50, 10, 10));
+    }
+
     public static void main(String[] args) {
         JFrame frame = new JFrame("Particle Simulation");
         frame.add(new GraphicsPanel(),BorderLayout.CENTER);
         frame.setSize(900,600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        for (Particle p : particles){
+            p.draw(g);
+        }
     }
 }
