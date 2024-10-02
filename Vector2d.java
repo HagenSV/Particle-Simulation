151c30aa65082a730a92f17ade966445aa665acc
CodingLegend0100 <117676748+HagenSV@users.noreply.github.com>
2024-10-01 20:02:25 -0400

    Change Vector2d design

--- Vector2d.java
+++ Vector2d.java
@@ -1,6 +1,6 @@
 public class Vector2d {
-    public final float x;
-    public final float y;
+    private float x;
+    private float y;
 
     public Vector2d(){
         x = 0;
@@ -12,15 +12,29 @@ public class Vector2d {
         this.y = y;
     }
 
+    public float getX(){ return x; }
+    public float getY(){ return y; }
+
+    public void setX(float x){ this.x = x; }
+    public void setY(float y){ this.y = y; }
+
+    public void set(float x, float y){ this.x = x; this.y = y; }
+
     public float getMagnitude(){
         return (float)Math.sqrt((x*x)+(y*y));
     }
 
-    public Vector2d add(Vector2d other){
-        return new Vector2d(this.x+other.x,this.y+other.y);
+    public void add(Vector2d other){
+        this.x += other.x;
+        this.y += other.y;
     }
 
-    public Vector2d multiply(float scalar){
-        return new Vector2d(this.x*scalar,this.y*scalar);
+    public void multiply(float scalar){
+        this.x *= scalar;
+        this.y *= scalar;
+    }
+
+    public Vector2d copy(){
+        return new Vector2d(this.x,this.y);
     }
 }
