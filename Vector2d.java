cef4de5aa97de02352f1bab9a03a35822cecb785
CodingLegend0100 <117676748+HagenSV@users.noreply.github.com>
2024-10-02 09:09:25 -0400

    More vector methods

--- Vector2d.java
+++ Vector2d.java
@@ -29,12 +29,35 @@ public class Vector2d {
         this.y += other.y;
     }
 
+    public void subtract(Vector2d other){
+        this.x -= other.x;
+        this.y -= other.y;
+    }
+
     public void multiply(float scalar){
         this.x *= scalar;
         this.y *= scalar;
     }
 
+    public void setMagnitude(float mag){
+        float dir = (float)Math.atan2(y, x);
+        x = (float)Math.cos(dir)*mag;
+        y = (float)Math.sin(dir)*mag;
+    }
+
+    public float dotProduct(Vector2d other){
+        return this.x*other.x + this.y*other.y;
+    }
+
     public Vector2d copy(){
         return new Vector2d(this.x,this.y);
+    }
+
+    public static Vector2d add(Vector2d a, Vector2d b){
+        return new Vector2d(a.x+b.x,a.y+b.y);
+    }
+
+    public static Vector2d subtract(Vector2d a, Vector2d b){
+        return new Vector2d(a.x-b.x, a.y-b.y);
     }
 }
